62bf8c855e325aeea42333810ab1318f483f73a6
Marco Pase <86538855+Mpase01@users.noreply.github.com>
2021-08-26 21:17:33 -0300

    Operadores Aritmeticos

---
+++ Operadores.py
@@ -0,0 +1,21 @@
+# Operadores Aritmeticos
+# Soma --> +
+# Subtração --> -
+# Divisão --> /
+# Divisão inteira --> //
+# Modulo --> %
+# Multiplicação --> *
+# Exponenciação --> **
+# Raiz Quadradra --> **(1/2)
+
+# Exemplo O.A
+n1 = int(input('Digite o primeiro numero: ')) 
+n2 = int(input('Digite o segundo numero: '))
+
+print('O resultado da soma é : {} '.format(n1 + n2)) 
+print('O resultado da subtração é: {} '.format(n1 - n2))
+print('O resultado da multiplicação é: {} '.format(n1 * n2))
+print('O resultado da divisão é: {} '.format(n1 / n2))
+print('O resultado da divisão inteira é: {} '.format(n1 // n2))
+print('O resultado da exponenciação é: {} '.format(n1 ** n2))
+print('O resultado do módulo é: {} '.format(n1 % n2))
