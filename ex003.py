2dd1326e6ff0c885f80d6649ed9b1167bc12bc9b
Marco Pase <86538855+Mpase01@users.noreply.github.com>
2023-08-05 19:57:34 -0300

    livro

--- ex003.py
+++ ex003.py
@@ -1,8 +1,12 @@
 # Usaremos um dos 4 tipos primitivos de variaveis para resolver esse exercicio, a variavel int que retorna um numero inteiro.
+
 # Para mostrarmos a soma de maneira mais adequada usaremos as {} seguindo do .format(variavel)
 
-n1 = int(input('Digite o primeiro numero: '))
-n2 = int(input('Digite o segundo numero: '))
+n1 = float(input('Digite o primeiro numero: '))
+n2 = float(input('Digite o segundo numero: '))
+
 s = n1 + n2
 
 print('A soma entre {} e {} é {}'.format(n1, n2, s))
+
+
